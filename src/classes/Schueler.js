594808b8ccd220cfdc0e7de9c914e17532c958e7
Thomas Klein <thomas.klein.1982@gmail.com>
2023-10-18 00:59:31 +0200

    nicht anwesende schueler

--- src/classes/Schueler.js
+++ src/classes/Schueler.js
@@ -7,6 +7,7 @@ export default class Schueler{
     this.arbeitsverhalten=null;
     this.sozialverhalten=null;
     this.noten=null;
+    this.nichtAnwesend=false;
   }
   static getSubFachData(data,index){
     let split=data.split("-------");
@@ -21,38 +22,53 @@ export default class Schueler{
   }
   calculateData(){
     this.anzeigeName=this.nachname+", "+this.vorname.split(" ")[0];
+    if(this.nichtAnwesend){
+      this.anzeigeName="("+this.anzeigeName+")";
+    }
   }
   parseFromExcel(reader,faecher){
     let data=reader.getCurrentCellContent();
     let pos=data.indexOf("-FachLehrer");
+    let nichtAnwesend=false;
+    if(pos<0){
+      pos=data.indexOf("-Fehlzeiten");
+      this.nichtAnwesend=true;
+    }
     let name=data.substring(0,pos).split(",");
     this.vorname=name[1].trim();
     this.nachname=name[0].trim();
     this.faecher=[];
-    for(let i=0;i<faecher.length;i++){
-      let fach=faecher[i];
-      //erst note:
-      data=reader.move(1,3);
-      //geteiltes Fach?
-      let s=Schueler.getSubFachData(data);
-      for(let j=0;j<s.length;j++){
-        let subfach=fach.getCopy();
-        subfach.setNote(s[j].trim());
-        data=reader.move(0,-1);
-        subfach.setArt(Schueler.getSubFachData(data,j));
-        data=reader.move(0,-1);
-        subfach.setFehlzeiten(Schueler.getSubFachData(data,j));
-        data=reader.move(0,-1);
-        if(!fach.hasLehrkraft()){
-          subfach.setLehrkraft(Schueler.getSubFachData(data,j));
+    if(!this.nichtAnwesend){
+      for(let i=0;i<faecher.length;i++){
+        let fach=faecher[i];
+        //erst note:
+        data=reader.move(1,3);
+        //geteiltes Fach?
+        let s=Schueler.getSubFachData(data);
+        for(let j=0;j<s.length;j++){
+          let subfach=fach.getCopy();
+          subfach.setNote(s[j].trim());
+          data=reader.move(0,-1);
+          subfach.setArt(Schueler.getSubFachData(data,j));
+          data=reader.move(0,-1);
+          subfach.setFehlzeiten(Schueler.getSubFachData(data,j));
+          data=reader.move(0,-1);
+          if(!fach.hasLehrkraft()){
+            subfach.setLehrkraft(Schueler.getSubFachData(data,j));
+          }
+          this.faecher.push(subfach);
+          reader.move(0,3);
         }
-        this.faecher.push(subfach);
-        reader.move(0,3);
+        reader.move(0,-3);
       }
-      reader.move(0,-3);
     }
+    
     reader.gotoLeft();
     this.calculateData();
-    return reader.move(0,5);
+    if(this.nichtAnwesend){
+      return reader.move(0,2);
+    }else{
+      return reader.move(0,5);
+    }
   }
 }
